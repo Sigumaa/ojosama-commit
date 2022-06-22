9354d27e86eab67bfe7482e8380d3418356cf67b
sigumaa <me@shiyui.dev>
2022-06-22 21:10:42 +0900

    messageを受け取ってcommitすることができるようになりましたわ。

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
-use std::process::Command;
+use std::{process::Command, str::FromStr};
+
+#[derive(Debug)]
+struct Arguments {
+    message: String,
+}
 
 fn main() {
-    let commit_message = "test message";
+    let args = parse_args();
+    let commit_message = args.message;
     Command::new("git")
         .arg("commit")
         .arg("-m")
         .arg(commit_message)
         .spawn()
         .expect("failed to start `git commit`");
+}
+
+fn parse_args() -> Arguments {
+    let mut args = Vec::new();
+
+    for arg in std::env::args().skip(1) {
+        args.push(String::from_str(&arg).expect("error parsing arguments"));
+    }
+
+    if args.len() != 1 {
+        eprintln!(
+            "Error: wrong number of argsments: expected 1, got {}.",
+            args.len()
+        );
+        eprintln!(r#"Try using """#);
+        std::process::exit(1);
+    };
+
+    Arguments {
+        message: args[0].to_string(),
+    }
 }
