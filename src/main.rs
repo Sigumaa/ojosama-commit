4a1993fbf8b9a6e2f35b79782faa970f211393be
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2023-08-29 09:56:37 +0900

    feat: ojosama error

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,49 @@ use serde::Serialize;
 use serde_json::json;
 use std::collections::HashMap;
 use std::process::Command;
-use std::process::ExitCode;
 
 #[derive(Debug, Serialize)]
 struct Arguments {
     message: String,
 }
 
+#[derive(Debug)]
+struct OjosamaError(String);
+
+impl std::fmt::Display for OjosamaError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "エラーが発生しましたわ。理由は以下の通りですわ。\n{}", self.0)
+    }
+}
+
+impl std::error::Error for OjosamaError {}
+
 #[tokio::main]
-async fn main() -> ExitCode {
-    let args = match parse_args() {
-        Ok(args) => args,
-        Err(e) => {
-            std::process::exit(e);
-        },
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = parse_args()?;
+    let commit_message = send_request(&args).await?;
+    commit_to_git(&commit_message)?;
+//
+    Ok(())
+}
+
+fn parse_args() -> Result<Arguments, Box<dyn std::error::Error>> {
+    let args = std::env::args().skip(1).collect::<Vec<_>>();
+
+    if args.len() != 1 {
+        let error_message = format!(
+            "引数の数が正しくありませんわ。引数の数は1つである必要がありますわ。与えられた引数は{}個でしたわ。",
+            args.len()
+        );
+        return Err(Box::new(OjosamaError(error_message)));
     };
 
+    let message = args[0].clone();
+
+    Ok(Arguments { message })
+}
+
+async fn send_request(args: &Arguments) -> Result<String, Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
     let res = client
         .post("https://api.ojosama.jiro4989.com")
@@ -26,50 +53,34 @@ async fn main() -> ExitCode {
             "Text": args.message,
         }))
         .send()
-        .await
-        .expect("リクエストに失敗してしまいましたわ。")
+        .await?
         .text()
-        .await
-        .expect("テキストをレスポンスから抽出できませんでしたわ。");
-    let res_text: HashMap<String, String> =
-        serde_json::from_str(&res).expect("かえってきたデータの処理に失敗してしまいましたわ。");
+        .await?
+        .trim()
+        .to_string();
+
+    let res_text: HashMap<String, String> = serde_json::from_str(&res)?;
 
     let commit_message = res_text
         .get("Result")
-        .expect("APIからかえってきたデータに異常がありましたわ。");
-        
+        .ok_or_else(|| OjosamaError("APIからかえってきたデータに異常がありましたわ。".to_string()))?
+        .clone();
 
+    Ok(commit_message)
+}
+
+fn commit_to_git(commit_message: &str) -> Result<(), Box<dyn std::error::Error>> {
     let is_success = Command::new("git")
-        .arg("commit")
-        .arg("-m")
-        .arg(commit_message)
-        .spawn()
-        .expect("`git commit`が実行できませんでしたわ。")
-        .wait()
-        .expect("`git commit`が実行されませんでしたわ。")
+        .args(&["commit", "-m", commit_message])
+        .spawn()?
+        .wait()?
         .success();
 
     if !is_success {
-        eprintln!("`git commit` に失敗してしまいましたわ。");
-        return ExitCode::FAILURE;
+        return Err(Box::new(OjosamaError(
+            "`git commit` に失敗してしまいましたわ。addはしましたの？".to_string(),
+        )));
     }
 
-    ExitCode::SUCCESS
-}
-
-fn parse_args() -> Result<Arguments, i32> {
-    let args = std::env::args().skip(1).collect::<Vec<_>>();
-
-    if args.len() != 1 {
-        eprintln!(
-            "引数の数が間違っておりますわ。 引数の数は1個であってほしいですわ。 実際の引数の数は {} 個でしたわ。",
-            args.len()
-        );
-        eprintln!(r#"ダブルクオーテーション "" で メッセージを囲ってみてくださいまし。"#);
-        return Err(1);
-    };
-
-    let message = args[0].clone();
-
-    Ok(Arguments { message })
+    Ok(())
 }
